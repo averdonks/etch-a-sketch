b8f803192fc252864ab46db2c7344988d9cd4dd9
averdonks <62974106+averdonks@users.noreply.github.com>
2025-02-22 12:09:51 -0500

    Add button functionality to create a new grid

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container");
 const containerWidth = +window.getComputedStyle(container).width.replace("px", "")
+const newGridBtn = document.querySelector(".newGridBtn");
 
 // Create a side x side grid with mouse interactivity
 function createGrid (side) {
-
     let i = 0;
     while (i < side) {
         createGridRow(side);
@@ -32,6 +32,25 @@ function createGrid (side) {
     }
 }
 
-// Testing
-let squaresPerSide = 16;
-createGrid(squaresPerSide);
+// Generate a new grid based on user input
+function newGrid () {
+    let keepGoing = true;
+    while (keepGoing) {
+        let squaresPerSide = +prompt("How many squares per side for the new grid? (min: 2 max: 100)", "16");
+        if (squaresPerSide <= 100 && squaresPerSide >= 2) {
+            keepGoing = false;
+                while (container.hasChildNodes()) {
+                container.removeChild(container.firstChild);
+                }
+                createGrid(squaresPerSide);
+        } else {
+            alert("Please enter a number between 2 and 100");
+        }
+    }
+}
+
+// Default grid
+createGrid(16);
+
+newGridBtn.addEventListener("click", newGrid)
+  
