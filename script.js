8e6d2a9f76a685f2dce3219d771f9fe7fc9941c0
averdonks <62974106+averdonks@users.noreply.github.com>
2025-02-22 09:52:48 -0500

    Add mouse "hover" functionality to draw squares

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
 const containerWidth = +window.getComputedStyle(container).width.replace("px", "")
-console.log();
-let squaresPerSide = 16;
 
+// Create a side x side grid with mouse interactivity
 function createGrid (side) {
 
     let i = 0;
@@ -16,9 +15,23 @@ function createGrid (side) {
             let square = document.createElement("div");
             square.classList.add("squareDefault");
             square.setAttribute(`style`, `width: ${containerWidth / side}; height: ${containerWidth / side};`);
+            addSquareEvents(square);
             container.appendChild(square);
         }
     }
+
+    function addSquareEvents (square) {
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("squareHover", "squareColor");
+            square.classList.remove("squareRemoveBorder");
+        });
+        square.addEventListener("mouseleave", () => {
+            square.classList.remove("squareHover");
+            square.classList.add("squareRemoveBorder");
+        });
+    }
 }
 
+// Testing
+let squaresPerSide = 16;
 createGrid(squaresPerSide);
